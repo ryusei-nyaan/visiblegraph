503c62fee0dbdfff5450a033f358ec7a7f7dfadb
ryuse-nyaan <r.s.iwasaki@ezweb.ne.jp>
2026-05-06 21:59:51 +0900

    fix:antigravityにやらせた.

--- visiblegraph/src/visible_graph.jl
+++ visiblegraph/src/visible_graph.jl
@@ -1,20 +1,27 @@
-using LightGraphs
+using Graphs
 using GraphPlot
 
-function visible_graph(x)
+"""
+    visible_graph(x::AbstractVector{T}) where T<:Real
+
+Convert a time series `x` into a visibility graph.
+Uses the O(N^3) algorithm.
+"""
+function visible_graph(x::AbstractVector{T}) where T<:Real
     N = length(x)
     g = SimpleGraph(N)
-    for i = 1:N
-        for j = i+1:N
+    for i in 1:N
+        for j in i+1:N
             cnt = 0
-            for k = i+1:j-1
-                if x[k]>=x[j]+(x[i]-x[j])*(j-k)/(j-i)
+            for k in i+1:j-1
+                # Visibility criterion
+                if x[k] >= x[j] + (x[i]-x[j]) * (j-k) / (j-i)
                     cnt = 1
                     break
                 end
             end
             if cnt == 0
-                add_edge!(g,i,j)
+                add_edge!(g, i, j)
             end
         end
     end
